148d01e58d1ccf24ff8c73a4512fe6daf6e1cc19
Alesia Branavitskaya <alesun4ik@gmail.com>
2023-03-15 00:06:55 +0300

    feat: add vitest and react testing library

---
+++ src/App.test.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import React from 'react';
+import App from './App';
+
+describe('App', () => {
+  it('Renders hello, world', () => {
+    // ARRANGE
+    render(<App />);
+    // ACT
+    // EXPECT
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Hello, world');
+  });
+});
