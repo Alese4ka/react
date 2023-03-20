f111fc4b3557ddd3206e47d627e12abce0f816f3
Alesia Branavitskaya <alesun4ik@gmail.com>
2023-03-20 14:58:02 +0300

    fix: adding basename to main.tsx for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter basename="/react">
+  <BrowserRouter basename="/react/">
     <App />
   </BrowserRouter>
 );
